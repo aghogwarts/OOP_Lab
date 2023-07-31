e5c220259fd391131fb51fbffe9dda5a3f4b4f55
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2023-07-31 10:27:03 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,22 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class Prime {
+    public static void main(String[] args) {
+        System.out.println("\n>> Program to check whether the number specified is Prime:\n");
+        int is_Prime = 1;
+        int n = 15;
+        for(int x = 2; x < n; x++){
+            if (n % x == 0){
+                is_Prime = 0;
+                break;
+            }
+        }
+        if (is_Prime == 0){
+            System.out.println("-- The number "+n+" is not Prime");
+        }
+        else {
+            System.out.println("-- The number "+n+" is Prime");
+        }
+    }
+}
