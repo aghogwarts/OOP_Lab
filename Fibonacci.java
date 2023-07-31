97dff5bb026006e37f0c5fb18da9a24f421d18dc
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2023-07-31 11:11:32 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println("\n>> Program to print the Fibonacci series upto 15 terms:\n");
+        int n_1 = 0;
+        int n_2 = 1;
+        int temp;
+        System.out.println(0);
+        System.out.println(1);
+        for (int x = 1; x < 13; x++){
+            temp = n_2;
+            n_2 += n_1;
+            n_1 = temp;
+            System.out.println(n_2);
+        }
+    }
+}
