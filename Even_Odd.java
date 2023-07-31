bf1bbca2937ff91a2ba868442bb6ad2b4c905f5e
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2023-07-31 10:26:31 +0530

    Create Even_Odd.java

---
+++ Even_Odd.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class Even_Odd {
+    public static void main(String[] args) {
+        System.out.println("\n>> Program to check whether the number specified is Even or Odd:\n");
+        int n = 15;
+        if (n % 2 == 0){
+            System.out.println("-- The number " + n + " is Even.");
+        }
+        else {
+            System.out.println("-- The number " + n + " is Odd.");
+        }
+    }
+}
